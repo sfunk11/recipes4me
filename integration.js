33e5544d36c0d66731c3f7ba120a1d7d7ea68883
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2020-12-14 20:05:51 -0500

    2nd API integration

--- integration.js
+++ integration.js
@@ -16,4 +16,38 @@ const settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+
+// The mealdb.com search by name
+let searchTerm = "curry";
+let queryURL= "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchTerm
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
+
+  // The mealdb.com search by main ingredient
+let searchIngredient = "chicken";
+let ingredientQueryURL= "https://www.themealdb.com/api/json/v1/1/search.php?i=" + searchIngredient
+
+$.ajax({
+    url: ingredientQueryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
+
+//  recipe puppy search by main ingredient or name
+
+let puppyQueryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + searchIngredient + "&q=" + searchTerm
+$.ajax({
+    url: puppyQueryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
 });
+
+
