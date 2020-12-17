f1e3c6bb5f76f9333136753c7ada845d56364648
Jonny Talbott <transverseponderer@gmail.com>
2020-12-16 21:19:50 -0500

    updating cookbook

--- scripts/integration.js
+++ scripts/integration.js
@@ -1,56 +1,54 @@
 $(document).ready(function(){
+// mycookbook.io
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi",
+	"method": "POST",
+	"headers": {
+		"content-type": "application/xml",
+		"x-rapidapi-key": "5c31647492msh818660de1066881p1c2b68jsn1ca367121afe",
+		"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com"
+	},
+	"data": "https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/"
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+  
+});
+
+// The mealdb.com search by name
+let searchTerm = "curry";
+let queryURL= "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchTerm
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
+
+  // The mealdb.com search by main ingredient
+let searchIngredient = "chicken";
+let ingredientQueryURL= "https://www.themealdb.com/api/json/v1/1/search.php?i=" + searchIngredient
+
+$.ajax({
+    url: ingredientQueryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
+
 //  recipe puppy search by main ingredient or name
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "5c31647492msh818660de1066881p1c2b68jsn1ca367121afe",
-            "x-rapidapi-host": "recipe-puppy.p.rapidapi.com"
-        }
-    };
-    
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-    // mycookbook.io
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://mycookbook-io1.p.rapidapi.com/recipes/rapidapi",
-// 	"method": "POST",
-// 	"headers": {
-// 		"content-type": "application/xml",
-// 		"x-rapidapi-key": "5c31647492msh818660de1066881p1c2b68jsn1ca367121afe",
-// 		"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com"
-// 	},
-// 	"data": "https://www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/"
-// };
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
-
-// // The mealdb.com search by name or ingredient
-// let searchTerm = "curry";
-// let searchIngredient = "chicken";
-// let queryURL= "https://www.themealdb.com/api/json/v1/1/search.php?"
-// if ($("#searchName").val().trim() !== ""){ 
-//     searchTerm = $("#searchName").val().trim();
-//     queryURL += "s=" + searchTerm;
-// }else
-//     if ($("#searchIngredient").val().trim() !== ""){
-//     searchIngredient = $("#searchIngredient").val().trim();
-//     queryURL += "i=" + searchIngredient;
-// }
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//       console.log(response);
-//   });
-
+let puppyQueryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + searchIngredient + "&q=" + searchTerm
+$.ajax({
+    url: puppyQueryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+  });
 });
 
 
