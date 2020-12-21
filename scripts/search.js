cd2b2ab8778dfd4a6de02f08633a990af9ebe00c
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2020-12-21 11:18:42 -0500

    now with search cards

--- scripts/search.js
+++ scripts/search.js
@@ -1,47 +1,55 @@
-$(document).ready(function () {
-    searchString = localStorage.getItem("searchTerm");
-    searchForRecipe(searchString);
+
     
-    function infoGen(results) {
+     function infoGen() {
+        let savedResults = JSON.parse(localStorage.getItem("searchResults"));
+        results = savedResults.results;
+        console.log(results);
+    
         for (var i = 0; i < 10; i++) {
-            var newCard = $("<div>").attr("class","card");
-            var imgEl = $("<img>").attr("id", "thumb");
-            //var img = $("#thumb").attr("src", results[i].thumbnail);
-            var cardTitleEl = $("<h4>").attr("id", "title");
-            //var cardTitle = $("#card-title").html(results[i].title);
-            var linkEl = $("<a>").attr("id", "link");
-           // var link = $("#link").attr("href", results[i].href);
-            var ingEl = $("<p>").attr("id", "ing");
-           // var ing = $("#ing").text("Ingredients: " + results[i].ingredients)
+            newCardTemplate = `<div class='card' style='width: 600px;'> <div class='card-divider' id='card-title-${i}'></div><img src='./assets/cook.jpg' id='thumb-${i}' style='height: 75px; width:75px; float: left;'> <div class='card-section' style='float: right;'><a href='#' id='link-${i}'>Link to website</a><p id='ing-${i}'></p></div> </div>`
+            var newCard = $(newCardTemplate);
+            $("#recipe-cards").append(newCard);
+            thumbID = "#thumb-" + i;
+            $(thumbID).attr("src", results[i].thumbnail);
+            titleID = "#card-title-" + i;
+            $(titleID).html(results[i].title);
+            linkID = "#link-" + i;
+           $(linkID).attr("href", results[i].href);
+            imgID = "#ing-" + i;
+            $(imgID).text("Ingredients: " + results[i].ingredients)
+        }};
 
-    let savedResults = JSON.parse(localStorage.getItem("searchResults"));
-    results = savedResults.results;
-
+ $(document).ready(function () {
+     searchString = localStorage.getItem("searchTerm");
+    searchForRecipe(searchString);
+     infoGen();  
+    })
     $("#searchBtn").on("click", function(){
         searchName = $("#searchName");
         ingredientInput = $("#searchIngredient");
         searchString = searchName.val().trim();
         ingString = ingredientInput.val().trim();
         searchForRecipe(searchString,ingString);
-    })
+        infoGen();
+   
 
 
-           // $("#card-title").append(cardTitle);
-            //$("#thumb").append(img);
-            //$("#link").append(link);
-            //$("#ing").append(ing);
-            newCard.append(cardTitleEl, imgEl, linkEl, ingEl);
-            $("#recipe-cards").append(newCard);
-            //}
-    }};
-    $("#search2").on("click", function () {
-        let savedResults = JSON.parse(localStorage.getItem("searchResults"));
-        var results = savedResults.results;
-        //for (i = 0; i < 10; i++) {
-            infoGen(results)
+        //    // $("#card-title").append(cardTitle);
+        //     //$("#thumb").append(img);
+        //     //$("#link").append(link);
+        //     //$("#ing").append(ing);
+        //     newCard.append(cardTitleEl, imgEl, linkEl, ingEl);
+        //     $("#recipe-cards").append(newCard);
+        //     //}
+    });
+    // $("#search2").on("click", function () {
+    //     let savedResults = JSON.parse(localStorage.getItem("searchResults"));
+    //     var results = savedResults.results;
+    //     //for (i = 0; i < 10; i++) {
+    //         infoGen(results)
             
-    })
-}
+
+
     
 
 
@@ -66,5 +74,5 @@ $(document).ready(function () {
     //      infoGen(20)
     // }
     //}
-);
+
 
