72e845e194c0bdd6da68898ddbcf5e9be1ac00a8
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2020-12-23 09:21:49 -0500

    added picture to cookbook and fixed recipe card

--- scripts/cookbook.js
+++ scripts/cookbook.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     for (var i = 0; i < recipeList.length; i++) {
      var recipeTable = $("<tr>");
      var recipeName = $("<td>");
+
      $(recipeName).attr("class", "button expanded recipeList");
      
      $(recipeName).attr("data-recipeList", i);
@@ -18,14 +19,15 @@ $(document).ready(function(){
 });
 
 function getingredients(ingredients){
- for (var i = 0; i < ingredients.length; i ++) {
+ $("#card-Ingredients").empty();
+    for (var i = 0; i < ingredients.length; i ++) {
      ingredientEl = $("<li>")
      $(ingredientEl).text(ingredients[i])
      $("#card-Ingredients").append(ingredientEl)
  }
 }
  function getinstructions(instructions){
-
+    $("#card-Instructions").empty();
     for (var i = 0; i < instructions[0].steps.length; i++) {
         instructionsEl = $("<li>")
         $(instructionsEl).text(instructions[0].steps[i])
@@ -35,6 +37,7 @@ function getingredients(ingredients){
 function displayrecipe(recipe){
    
     $(".card-name").text(recipe.name);
+    $("#card-Image").attr("src", recipe.images[0]);
     $("#link").attr("href", recipe.recipeURL);
     $("#card-description").text(recipe.description);
     getingredients(recipe.ingredients);
@@ -56,18 +59,3 @@ $("#searchBtn").on("click", function(){
 });
 
         
-// function createRecipeCard(name, images, ingredients, instructions,) {
-// let recipeCardEl = $("<div>").attr("class", "recipe-Card");
-// let cardName = $("<h3>").attr("class", "card-name");
-// let cardImages = $("<img>").attr("class", "images");
-// let cardDescription = $("<p>").attr("id", "card-description");
-// let cardIngredients = $("<p>").attr("id", "card-Ingredients");
-// let cardInstructions = $("<p>").attr("id", "card-Instructions");
-
-// cardRow.append(recipeCardEl);
-// cardName.text(recipe);
-// cardImages.attr("src", icon);
-// cardDescription.text(`Description: ${description}`);
-// cardIngredients.text(`Ingredients: ${ingredients}`);
-// cardInstructions.text(`Instructions: ${instructions}`);
-// recipeCardEl.append(cardName, cardImages, cardDescription, cardIngredients, cardInstructions);
